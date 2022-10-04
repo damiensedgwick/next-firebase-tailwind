76cee8de1c96ef157c623c0fcef3f4e141a1eb94
Damien Sedgwick <damiensedgwick@icloud.com>
2022-10-04 22:03:13 +0100

    added navigation links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <header>
-      <h1>Header</h1>
+      <h1>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </h1>
+
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/dashboard">
+            <a>Dashboard</a>
+          </Link>
+        </li>
+      </ul>
     </header>
   );
 };
