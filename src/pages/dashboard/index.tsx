9fc9447f90709b02ed92ee1dbe5976775e0ddb3e
Damien Sedgwick <damiensedgwick@icloud.com>
2022-10-05 09:08:28 +0100

    added protected route text to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -6,7 +6,11 @@ const Dashboard: NextPage = () => {
     <Layout>
       <main className="p-4">
         <div className="container mx-auto">
-          <h1>Hello, Dashboard!</h1>
+          <h1 className="text-2xl text-center">Your Dashboard</h1>
+          <p className="mt-4 text-lg text-center">
+            ⚠️⚠️ This should be a protected route and only accessible when there
+            is a user ⚠️⚠️
+          </p>
         </div>
       </main>
     </Layout>
