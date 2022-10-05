38190b8b57cc34a51cb46b5ac4fd06d9dc541ac4
Damien Sedgwick <damiensedgwick@icloud.com>
2022-10-05 09:20:43 +0100

    added scaffolding for auth in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import "styles/globals.css";
 
-import '../styles/globals.css'
+const protectedRoutes = ["/dashboard"];
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <div>
+      {protectedRoutes.includes(router.pathname) ? (
+        <div>
+          <Component {...pageProps} />
+        </div>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
