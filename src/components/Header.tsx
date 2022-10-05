cb1e6613656e5847bbe959e793a3a2a88c07b68e
Damien Sedgwick <damiensedgwick@icloud.com>
2022-10-05 08:44:46 +0100

    added styling for Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,30 +2,32 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header>
-      <h1>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </h1>
-
-      <ul>
-        <li>
+    <div className="py-4  border-b-2 border-gray-900">
+      <header className="container mx-auto flex flex-row items-center justify-between">
+        <h1>
           <Link href="/">
             <a>Home</a>
           </Link>
-        </li>
-        <li>
-          <Link href="/login">
-            <a>Login</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dashboard">
-            <a>Dashboard</a>
-          </Link>
-        </li>
-      </ul>
-    </header>
+        </h1>
+
+        <ul className="flex flex-row items-center justify-between">
+          <li className="px-4">
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li className="px-4">
+            <Link href="/login">
+              <a>Login</a>
+            </Link>
+          </li>
+          <li className="px-4 border-l-2 border-gray-900">
+            <Link href="/dashboard">
+              <a>Dashboard</a>
+            </Link>
+          </li>
+        </ul>
+      </header>
+    </div>
   );
 };
